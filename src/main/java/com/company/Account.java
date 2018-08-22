e592c17c9bc3b52172723a6c03c7f2c18103c434
sparrow84 <18346183461q>
2018-08-22 23:45:52 +0700

    На середине урока

--- src/main/java/com/company/Account.java
+++ src/main/java/com/company/Account.java
@@ -4,19 +4,56 @@ import lombok.Data;
 
 @Data
 public class Account {
+
+
+    private static long count;
     private final long id;
     private String fio;
-    private double amount;
+    protected double amount;
+
 
     public Account(long id) {
         this.id = id;
     }
 
+    public Account() {
+        this(0);
+        System.out.println("Call def constructor");
+    }
+
+//    public String getFio() {
+//        return fio;
+//    }
+//
+//    public void setFio(String fio) {
+//        this.fio = fio;
+//    }
+//
+//    public double getAmount() {
+//        return amount;
+//    }
+//
+//    public void setAmount(double amount) {
+//        this.amount = amount;
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public static long getCount() {
+//        return count;
+//    }
+//
+//    public static void setCount(long count) {
+//        Account.count = count;
+//    }
+
     public void addValue(double v) {
         this.amount += v;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Account account1 = new Account(1);
         account1.setFio("Ivan");
         account1.setAmount(1500);
